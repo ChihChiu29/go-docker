77ca66fb5dbd1069c9ac2abcf386cb91f732b4be
ChihChiu29 <chih.chiu.19@gmail.com>
2017-08-17 22:35:32 -0700

    add go.sh?

---
+++ go.sh
@@ -0,0 +1,51 @@
+#! /usr/bin/env bash
+
+# Golang Docker image name.
+GO_IMAGE=golang
+
+os=""
+arch=""
+
+function print_usage {
+  echo 'Usage: godocker <go sub-commands>'
+  echo 'Run this script under the "go root" folder, which contains sub-folders "src", "pkg", and "bin".'
+  echo 'Example:'
+  echo '1) Build src/main.go:'
+  echo 'godocker install main'
+}
+
+function checked_detect_os_and_arch {
+  case $(uname) in
+    "Darwin")
+      os="darwin"
+      ;;
+    "Linux")
+      os="linux"
+      ;;
+    "Windows")
+      os="Windows"
+      ;;
+  esac
+
+  arch_raw=$(uname -m)
+  if [[ "$arch_raw" == *"386"* ]]; then
+    arch="386"
+  fi
+  if [[ "$arch_raw" == *"64"* ]]; then
+    arch="amd64"
+  fi
+
+  if [[ "$os" == "" || "$arch" == "" ]]; then
+    echo "Error: cannot detect OS and/or archtitecture."
+    exit -1
+  fi
+}
+
+if [[ $# -lt 1 ]]; then
+  print_usage
+  exit -1
+fi
+
+checked_detect_os_and_arch
+# docker run --rm -v "$PWD":/go/ -w /go/ -e GOOS="$os" -e GOARCH="$arch" golang go build -v "$package_path"
+docker run --rm -v "$PWD":/go/ -w /go/ -e GOOS="$os" -e GOARCH="$arch" golang go "$@"
